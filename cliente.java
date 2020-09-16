bb5b0cd915832dae169742118940798c528961df
ValenAle612 <valenale612@gmail.com>
2020-09-16 19:34:21 -0300

    segundo commit

--- cliente.java
+++ cliente.java
@@ -1,6 +1,6 @@
 
 public class cliente {
 	public static void main(String args[]) {
-		persona p=new persona("Martin", "Larrea", 40, 25004560);
+		persona p=new persona("Jose", "Larrea", 40, 25004560);
 	}
 }
