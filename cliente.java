6936437d0e732b3e68f770d9e1761174969e0bbf
ValenAle612 <valenale612@gmail.com>
2020-09-16 17:54:06 -0300

    prueba inicial

---
+++ cliente.java
@@ -0,0 +1,6 @@
+
+public class cliente {
+	public static void main(String args[]) {
+		persona p=new persona("Martin", "Larrea", 40, 25004560);
+	}
+}
---
+++ persona.java
@@ -0,0 +1,13 @@
+
+public class persona {
+	private String nombre, apellido;
+	private int edad;
+	private long dni;
+	
+	public persona(String nombre, String apellido, int edad, long dni) {
+		this.nombre=nombre;
+		this.apellido=apellido;
+		this.edad=edad;
+		this.dni=dni;
+	}
+}
